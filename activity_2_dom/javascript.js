30dba3986d28c5a8e06ceaa0460edec07844449d
Jermin Odcheo <jerminsux6@gmail.com>
2023-10-12 20:30:28 +0800

    Added Anime Information

--- activity_2_dom/javascript.js
+++ activity_2_dom/javascript.js
@@ -1,71 +1,89 @@
 class AnimeSearcher {
   constructor() {
-    this.config = {
-      animeInput: "animeInput",
-      genreSelect: "genreSelect",
-      typeSelect: "typeSelect",
-      yearInput: "yearInput",
-      ratingSelect: "ratingSelect",
-      statusSelect: "statusSelect",
-      countrySelect: "countrySelect",
-      seasonSelect: "seasonSelect",
-      resultsDiv: "results",
-      searchBtn: "searchBtn",
+    this.elements = {
+      animeInput: document.getElementById("animeInput"),
+      genreSelect: document.getElementById("genreSelect"),
+      typeSelect: document.getElementById("typeSelect"),
+      yearInput: document.getElementById("yearInput"),
+      ratingSelect: document.getElementById("ratingSelect"),
+      statusSelect: document.getElementById("statusSelect"),
+      countrySelect: document.getElementById("countrySelect"),
+      seasonSelect: document.getElementById("seasonSelect"),
+      resultsDiv: document.getElementById("results"),
+      searchBtn: document.getElementById("searchBtn"),
     };
 
-    for (let key in this.config) {
-      this[key] = document.getElementById(this.config[key]);
-    }
-
-    this.searchBtn.addEventListener("click", this.search.bind(this));
+    this.elements.searchBtn.addEventListener("click", this.search.bind(this));
   }
 
-  createAnimeEntry(anime) {
+  createAnimeCard(anime) {
     const animeDiv = document.createElement("div");
     animeDiv.classList.add("anime-container");
 
-    const animeTitle = document.createElement("h2");
-    animeTitle.innerText = anime.title || "Unknown Title";
-    animeDiv.appendChild(animeTitle);
+    // Displaying the title
+    const title = document.createElement("h2");
+    title.innerText = anime.title || "Unknown Title";
+    animeDiv.appendChild(title);
 
+    // Displaying the image
+    if (anime.image_url) {
+      const img = document.createElement("img");
+      img.src = anime.image_url;
+      img.alt = `Image of ${anime.title}`;
+      img.onerror = function () {
+        this.onerror = null;
+        this.src = "path_to_fallback_image.jpg";
+      };
+      animeDiv.appendChild(img);
+    }
+
+    // Displaying all other details (as an example, add more as required)
+    const synopsis = document.createElement("p");
+    synopsis.innerText = anime.synopsis || "No synopsis available.";
+    animeDiv.appendChild(synopsis);
 
     return animeDiv;
   }
 
   displayResults(data) {
-    this.resultsDiv.innerHTML = "";
-
+    this.elements.resultsDiv.innerHTML = "";
     if (!data.data || data.data.length === 0) {
-      this.resultsDiv.innerHTML = "No Result";
+      this.elements.resultsDiv.innerHTML = "No Result";
       return;
     }
 
     data.data.forEach((anime) => {
-      this.resultsDiv.appendChild(this.createAnimeEntry(anime));
+      const animeCard = this.createAnimeCard(anime);
+      this.elements.resultsDiv.appendChild(animeCard);
     });
-
-
-    
   }
 
-  getURL() {
-    const baseURL = "https://api.jikan.moe/v4/anime?";
-    let queryParams = [
-      this.animeInput.value.trim() && `q=${this.animeInput.value.trim()}`,
-      this.genreSelect.value && `genres=${this.genreSelect.value}`,
-      this.typeSelect.value && `type=${this.typeSelect.value}`,
-      this.yearInput.value && `year=${this.yearInput.value}`,
-      this.ratingSelect.value && `rating=${this.ratingSelect.value}`,
-      this.statusSelect.value && `status=${this.statusSelect.value}`,
-      this.countrySelect.value && `country=${this.countrySelect.value}`,
-      this.seasonSelect.value && `season=${this.seasonSelect.value}`,
-    ].filter(Boolean);
-    console.log(baseURL + queryParams.join("&"));
-    return baseURL + queryParams.join("&");
+  getSearchURL() {
+    const params = [
+      this.elements.animeInput.value.trim() &&
+        `q=${this.elements.animeInput.value.trim()}`,
+      this.elements.genreSelect.value &&
+        `genre=${this.elements.genreSelect.value}`,
+      this.elements.typeSelect.value &&
+        `type=${this.elements.typeSelect.value}`,
+      this.elements.yearInput.value && `year=${this.elements.yearInput.value}`,
+      this.elements.ratingSelect.value &&
+        `rating=${this.elements.ratingSelect.value}`,
+      this.elements.statusSelect.value &&
+        `status=${this.elements.statusSelect.value}`,
+      this.elements.countrySelect.value &&
+        `country=${this.elements.countrySelect.value}`,
+      this.elements.seasonSelect.value &&
+        `season=${this.elements.seasonSelect.value}`,
+    ]
+      .filter(Boolean)
+      .join("&");
+
+    return `https://api.jikan.moe/v4/anime?${params}`;
   }
 
   search() {
-    fetch(this.getURL())
+    fetch(this.getSearchURL())
       .then((res) => res.json())
       .then((data) => {
         this.displayResults(data);
@@ -75,4 +93,4 @@ class AnimeSearcher {
   }
 }
 
-const searcher = new AnimeSearcher();
+const animeSearcher = new AnimeSearcher();
