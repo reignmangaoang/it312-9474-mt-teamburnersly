f9d6c7609d61905e285bfec7d884fb7c2f5b4fb2
RedconAlice <98995728+RedconAlice@users.noreply.github.com>
2023-10-15 19:47:27 +0800

    Updated Project

---
+++ activity_1_json/scripts/Login.js
@@ -0,0 +1,15 @@
+async function userExists(){
+    let users = await fetch("Data/Users.json")
+    let userList = await users.json()
+    for(let i = 0; i <userList.length; i++){
+        var user = userList[i]
+        var userName = document.getElementById("username").value
+        var password = document.getElementById("password").value
+        if(user.userName==userName&&user.password==password){
+            localStorage.setItem("User ID",user.userid)
+            window.location.href="profile.html"
+            return
+        }
+    }
+    console.log("False")    
+}
